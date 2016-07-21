73110bb90543c79c4d6964e9e0ab6e32483ba452
Changhwa Oh <changhwaoh.co@gmail.com>
2016-07-21 19:52:45 +0900

    feat: 게시글 리스트 화면

--- webapp/src/components/board/Board.js
+++ webapp/src/components/board/Board.js
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
-import { Panel } from 'react-bootstrap';
+import request from 'superagent';
+import { Grid, Col } from 'react-bootstrap';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import 'react-bootstrap-table/css/react-bootstrap-table-all.min.css';
 
 export default class Board extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      articles: []
+    };
+  }
+
+  componentWillMount() {
+    this.loadArticles();
+  }
+
+  loadArticles() {
+    request.get('/api/article')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (!err) {
+          this.setState({
+            articles: res.body.content
+          });
+        }
+      });
   }
 
   render() {
     return (
-      <Panel style={{ width: '500', margin: '0 auto' }}>
-        <div className="text-center">
-          <h4 className="content-group">게시판!</h4>
-        </div>
-        <hr />
-        <div>
-          Hello World
-        </div>
-      </Panel>
+      <div>
+        <Grid fluid={true}>
+          <h2>게시판</h2>
+          <hr/>
+          <Col xs={12}>
+            <div>
+              <BootstrapTable data={this.state.articles}>
+                <TableHeaderColumn dataField="id" isKey={true} hidden>ID</TableHeaderColumn>
+                <TableHeaderColumn dataField="title">제목</TableHeaderColumn>
+              </BootstrapTable>
+            </div>
+          </Col>
+        </Grid>
+      </div>
     );
   }
 }
