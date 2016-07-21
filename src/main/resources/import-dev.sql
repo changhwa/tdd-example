0fe6d85488fe3a660e97470d8b81de99bee14037
Changhwa Oh <changhwaoh.co@gmail.com>
2016-07-21 19:54:56 +0900

    chore: 테스트를 위한 테스트 데이터 추가

---
+++ src/main/resources/import-dev.sql
@@ -0,0 +1,11 @@
+insert into article (id, title, body) VALUES (1, '테스트-제목1', '테스트-본문1');
+insert into article (id, title, body) VALUES (2, '테스트-제목2', '테스트-본문2');
+insert into article (id, title, body) VALUES (3, '테스트-제목3', '테스트-본문3');
+insert into article (id, title, body) VALUES (4, '테스트-제목4', '테스트-본문4');
+insert into article (id, title, body) VALUES (5, '테스트-제목5', '테스트-본문5');
+insert into article (id, title, body) VALUES (6, '테스트-제목6', '테스트-본문6');
+insert into article (id, title, body) VALUES (7, '테스트-제목7', '테스트-본문7');
+insert into article (id, title, body) VALUES (8, '테스트-제목8', '테스트-본문8');
+insert into article (id, title, body) VALUES (9, '테스트-제목9', '테스트-본문9');
+
+--
